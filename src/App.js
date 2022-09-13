83794a53815bfaa979d522b33f6ed1ca5053831f
salwanurra <salwanurs09@gmail.com>
2022-09-13 21:26:54 +0700

    add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css"
+import Header from './components/Header';
 
 function App() {
   return (
     <>
+    <Header />
       <div className="p-5 mb-4 bg-light rounded-3">
         <div className="container-fluid py-5">
           <h1 className="display-5 fw-bold">First Paragraph in Jumbroton</h1>
